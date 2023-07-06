a94e34da4e9d6e9bfd4dad87707b1b06d3d5784a
Beka <azamatovbekmamat518@gmail.com>
2023-07-06 13:11:40 +0600

    added methods Person Student

--- src/service/Person.java
+++ src/service/Person.java
@@ -1,4 +1,8 @@
 package service;
 
 public interface Person {
+    void addPerson();
+    void findByID();
+    void updatePerson();
+    void getAllPersons();
 }
--- src/service/Student.java
+++ src/service/Student.java
@@ -1,4 +1,8 @@
 package service;
 
 public interface Student {
+    void addStudents();
+    void findByID();
+    void updateStudent();
+    void getAllStudents();
 }
